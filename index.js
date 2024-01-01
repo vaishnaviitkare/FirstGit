744b8b39bed42b53e77a5bc1686265cd5a2b7d3d
vaishnaviitkare <vaishuitkare@gmail.com>
2024-01-01 15:08:15 +0530

    use query selector and query selector all

--- index.js
+++ index.js
@@ -21,3 +21,29 @@ list[4].style.color='blue';
 for(let i=0;i<list.length;i++){
   list[i].style.fontStyle='italic';
 }
+//query selector and query selectorAll
+const basket=document.querySelector('#basket-heading');
+basket.style.color='brown';
+basket.style.marginLeft='30px';
+const fruits=document.querySelectorAll('.fruits');
+for(let i=0;i<fruits.length;i++){
+fruits[i].style.backgroundColor='grey';
+fruits[i].style.borderRadius='5px';
+fruits[i].style.padding='30px';
+fruits[i].style.listStyleType='none';
+fruits[i].style.margin='30px';
+fruits[i].style.width='50%';
+  }
+
+const fruit=document.querySelectorAll('.fruit');
+for(let i=0;i<fruit.length;i++){
+  fruit[i].style.backgroundColor='white';
+  fruit[i].style.padding='10px';
+  fruit[i].style.margin='10px';
+  fruit[i].style.borderRadius='5px';
+ }
+const even=document.querySelectorAll('.fruit:nth-child(even)');
+for(let i=0;i<even.length;i++){
+even[i].style.backgroundColor='brown';
+even[i].style.color='white';
+}
