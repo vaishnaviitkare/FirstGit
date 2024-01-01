bb6ceb5a5e8eb9508abbd390854287e09f09877d
vaishnaviitkare <vaishuitkare@gmail.com>
2024-01-01 13:46:49 +0530

    use getElementByTagName

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//getElementById
 const mainHeading=document.getElementById('main-heading');
 mainHeading.textContent='Fruit World';
 mainHeading.style.color='orange';
@@ -8,10 +9,15 @@ const basket=document.getElementById('basket-heading');
 basket.style.color='green';
 const thanks=document.getElementById('thanks');
 thanks.innerHTML='<p>Please visit us again</p>';
-
-
+//getElementsByClassName
 const fru=document.getElementsByClassName('fruit');
 fru[2].style.backgroundColor='yellow';
 for(let i=0;i<fru.length;i++){
   fru[i].style.fontWeight='bold'
+}
+//getElementsByTagName
+const list=document.getElementsByTagName('li');
+list[4].style.color='blue';
+for(let i=0;i<list.length;i++){
+  list[i].style.fontStyle='italic';
 }
