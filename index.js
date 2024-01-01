77abc7046b057a0b8d4e76430ad3d8d503fb61a1
vaishnaviitkare <vaishuitkare@gmail.com>
2024-01-01 12:44:38 +0530

    two files created

---
+++ index.js
@@ -0,0 +1,12 @@
+const mainHeading=document.getElementById('main-heading');
+mainHeading.textContent='Fruit World';
+mainHeading.style.color='orange';
+const head=document.getElementById('header');
+head.style.backgroundColor='green';
+head.style.borderBottom='3px solid orange';
+const basket=document.getElementById('basket-heading');
+basket.style.color='green';
+const thanks=document.getElementById('thanks');
+thanks.innerHTML='<p>Please visit us again</p>';
+
+
