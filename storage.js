d286f0232879ee79fe17aef4e865aa5e66f3ea71
vaishnaviitkare <vaishuitkare@gmail.com>
2024-01-09 15:18:47 +0530

    instead of individually storing fields, we stored data as object in local storage

--- storage.js
+++ storage.js
@@ -3,9 +3,40 @@ function storage(event){
        const name=event.target.username.value;
        const email=event.target.emailId.value;
        const phonenumber=event.target.phonenumber.value;
+      /*
        //storing in local storage
        localStorage.setItem('Name',name);
        localStorage.setItem('Email',email);
        localStorage.setItem('PhoneNumber',phonenumber);
+       */
+      //also we stored this data as an object in local storage
+        //creating object
+        const obj={
+          Name:name,
+          Email:email,
+          Phone:phonenumber
+          /*also we do this
+          const obj={
+        name,
+        email,
+        phonenumber
+          }
+        //it means 
+        name:name,
+        email:email,
+        phonenumber:phonenumber
+        */
+        };
+        //convert obj to string
+        const storedUserDetails=JSON.stringify(obj);
+        localStorage.setItem('userDetails',storedUserDetails);
+        //also we do
+        //localStorage.setItem('userDetails',JSON.stringify(obj));
+      
+        //convert string to obj
+        //const get=JSON.parse(localStorage.getItem("userDetails")));
+      
+
+
 }
 
